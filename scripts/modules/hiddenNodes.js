571285a5449df6a8bb34ceba27239a884fd47d32
Steffan Long <Steffwiz@me.com>
2015-10-23 02:15:22 -0700

    add names for hidden nodes and remove outdated node

--- scripts/modules/hiddenNodes.js
+++ scripts/modules/hiddenNodes.js
@@ -1,35 +1,34 @@
 module.exports = [
-  74523350,
-  193091484,
+  74523350,   // Cannibalism
+  193091484,  // Increase Strength
   213547364,  // Will of Light
   217480046,  // Twist Fate
-  300289986,
-  431265444,
-  472357138,
-  617082448,
-  643689081,
-  994456416,
-  1034209669,
-  1263323987,
-  1270552711,
-  1305317488,
-  1450441122,
-  1644354530,
-  1782221257,
-  1891493121,
-  1920788875,
-  1975859941,
-  2086308543,
-  2470010183,
-  2636878840,
-  2688431654,
-  2689436406,
-  2845051978,
-  2978058659,
-  3200611139,
-  3575189929, // Hands-on
-  3707521590,
-  3742851299,
-  3985040583,
+  300289986,  // Dreg Burn
+  431265444,  // Mutineer
+  472357138,  // Void Damage
+  617082448,  // Reforge Ready
+  643689081,  // Kinetic Damage
+  994456416,  // Burgeoning Hunger
+  1034209669, // Increase Intellect
+  1263323987, // Increase Discipline
+  1270552711, // Infuse
+  1305317488, // Aspect Swap
+  1450441122, // Demotion
+  1644354530, // Sword Strike
+  1782221257, // Shank Burn
+  1891493121, // Dark Breaker
+  1920788875, // Ascend
+  1975859941, // Solar Damage
+  2086308543, // Upgrade Defense
+  2470010183, // Hive Disruptor
+  2688431654, // Arc Damage
+  2689436406, // Upgrade Damage
+  2845051978, // Ice Breaker
+  2978058659, // Oracle Disruptor
+  3200611139, // Scabbard
+  3575189929, // Hands-On
+  3707521590, // Vandal Burn
+  3742851299, // Lich Bane
+  3985040583, // Disciplinarian
   4197414939  // Inverse Shadow
 ];
